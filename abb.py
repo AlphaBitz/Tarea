4e04bb8bc844ad4137e5639bb47044c1a408f40e
Nicolás Román <39896127+AlphaBitz@users.noreply.github.com>
2018-06-25 22:57:27 -0400

    ABB.py
    V1.0 ABB:
    Función Insertar, Imprimir(Inorder, orden alfabético), Eliminar  y buscar.

--- abb.py
+++ abb.py
@@ -1,69 +1,73 @@
-# -*- coding: utf-8 -*-
-
 class Node:
-    def __init__(self, value):
+    def __init__(self, nombre,apellido,email,numero):
         self.left = None
         self.right = None
-        self.value = value
-        self.parent = None # Añadimos el atributo parent para facilitar la eliminación de un nodo
+        self.nombre=nombre
+        self.apellido = apellido
+        self.email=email
+        self.numero=numero
+        self.parent = None 
+    def get_info(self):
+      print (self.nombre,self.apellido,self.email,self.numero)
+      return
 
-class BST:
+class ABB:
     def __init__(self):
         self.root = None
-
     def empty(self):
         return self.root == None
-
-    def _insert(self, value, node):
-        if value < node.value:
+    def _insertar(self,nombre,apellido,email,numero , node):
+        if apellido < node.apellido:
             if node.left == None:
-                node.left = Node(value)
+                node.left = Node(nombre,apellido,email,numero)
                 node.left.parent = node
             else:
-                self._insert(value, node.left)
-        elif value > node.value:
+                self._insertar(nombre,apellido,email,numero , node.left)
+        elif apellido > node.apellido:
             if node.right == None:
-                node.right = Node(value)
+                node.right = Node(nombre,apellido,email,numero)
                 node.right.parent = node
             else:
-                self._insert(value, node.right)
+                self._insertar(nombre,apellido,email,numero, node.right)
         else:
-            print("Value is already in the Tree")
-
-    def insert(self, value):
+            print("El apellido ya a sido ingresado")
+    def insertar(self, nombre,apellido,email,numero):
         if self.empty():
-            self.root = Node(value)
+            self.root = Node(nombre,apellido,email,numero)
         else:
-            self._insert(value, self.root)
-
-    def _find(self, value, node):
-        if node == None:
-            return None
-        elif value == node.value:
-            return node
-        elif value < node.value and node.left != None:
-            return self._find(value, node.left)
-        elif value > node.value and node.right != none:
-            return self._find(value, node.right)
-
-    def find(self, value):
+            self._insertar(nombre,apellido,email,numero, self.root)
+    def _buscar(self, apellido, node):
+        if node.apellido == None:
+           return node 
+        elif apellido == node.apellido:
+            print ("Nodo Encontrado:")
+            node.get_info()
+            return node 
+        elif apellido < node.apellido and node.left != None:
+            return self._buscar(apellido, node.left)
+        elif apellido > node.apellido and node.right != None:
+            return self._buscar(apellido, node.right)
+        print("No encontrado")
+    def buscar(self, apellido):
         if self.empty():
-            return None
+          print ("Sin Raiz")
+          return None
+
         else:
-            return self._find(value, self.root)
-
-    def delete(self, value): #Implementar
+            return self._buscar(apellido, self.root)
+    def eliminar(self, apellido): 
         if self.empty():
+            print ("Sin Raiz")
             return None
-        return self.delete_node(self.find(value))
+        return self.eliminar_nodo(self.buscar(apellido))
 
-    def delete_node(self, node):
+    def eliminar_nodo(self, node):
         def min_value_node(n):
             current = n
             while current.left != None:
                 current = current.left
             return current
-        def number_children(n): # Return the number of childrens of the node to be deleted
+        def number_children(n):
             number_children = 0
             if n.left != None:
                 number_children += 1
@@ -71,129 +75,56 @@ class BST:
                 number_children += 1
             return number_children
 
-        node_parent = node.parent # Get the parent of the node to be deleted
+        node_parent = node.parent 
         node_children = number_children(node)
 
-        # Case 1: Deleting a node without childrens
         if node_children == 0:
+            print ("Sin padres")
             if node_parent.left == node:
                 node_parent.left = None
             else:
                 node_parent.right = None
-        # Case 2: Deleting a node with one children
         if node_children == 1:
-            # Get the children of the node to be deleted
+            print ("1 Hijo")
             if node.left != None:
                 child = node.left
             else:
                 child = node.right
-
-            # Replace the node to be deleted with its child
             if node_parent.left == node:
                 node_parent.left = child
             else:
                 node_parent.right = child
-
-            # Change the parent of the child
             child.parent = node_parent
-        # Case 3: Deleting a node with two childrens
         if node_children == 2:
-            successor = min_value_node(node.right) # Get the inorder successor of the deleted node
-            node.value = successor.value # Copy the value
-            self.delete_node(successor)
+            print ("2 Hijos")
+            successor = min_value_node(node.right)
+            node.apellido = successor.apellido 
+            self.eliminar_nodo(successor)
 
-    def in_order(self, node): #Implementar
+    def imprimir(self, node): #Implementar
+        if node==None:
+            pass
+        else:
+            self.imprimir(node.left)
+            node.get_info()
+            self.imprimir(node.right)
+
+    """def post_order(self, node): #Implementar
         if node==None:
             pass
         else:
             self.in_order(node.left)
-            print(node.value)
             self.in_order(node.right)
-
-    def post_order(self, node): #Implementar
-        if node==None:
-            pass
-        else:
-            self.in_order(node.left)
-            self.in_order(node.right)
-            print(node.value)
+            node.get_info()
 
     def pre_order(self, node): #Implementar
         if node==None:
             pass
         else:
-            print(node.value)
+            node.get_info()
             self.in_order(node.left)
-            self.in_order(node.right)
-
-    def leaf_number(self): #Implementar
-        pass
+            self.in_order(node.right)"""
 
 
-    def _tree_height(self, current, height):
-        if current == None:
-            return height
-        left_height = self._tree_height(current.left, height+1)
-        right_height = self._tree_height(current.right, height+1)
-        return max(left_height, right_height)
 
-    def tree_height(self): #Implementar
-        if self.empty():
-            return 0
-        else:
-            return self._tree_height(self.root, 0)
-
-    def node_height(self, value): #Implementar
-        node = self.root
-        height = 0
-        if self.find(value):
-            height += 1
-            while node.value != value:
-                if value < node.value:
-                    node = node.left
-                else:
-                    node = node.right
-                height += 1
-            return height
-        return False
-
-    def find_minimum(self): #Implementar
-        if self.empty():
-            return None
-        else:
-            current = self.root
-            while current.left is not None:
-                current = current.left
-            return current
-
-    def find_maximum(self): #Implementar
-        if self.empty():
-            return None
-        else:
-            current = self.root
-            while current.right is not None:
-                current = current.right
-            return current
-
-if __name__=="__main__":
-    bst = BST()
-    bst.insert(10)
-    bst.insert(12)
-    bst.insert(5)
-    bst.insert(4)
-    bst.insert(20)
-    bst.insert(8)
-    bst.insert(7)
-    bst.insert(15)
-    bst.insert(13)
-    print("=== In order ===")
-    bst.in_order(bst.root)
-    print("=== end in order ===")
-    print("Min: ", bst.find_minimum().value)
-    print("Max: ", bst.find_maximum().value)
-    print("Node height 4: ", bst.node_height(4))
-    print("Tree height: ", bst.tree_height())
-    bst.delete(5)
-    print("=== In order ===")
-    bst.in_order(bst.root)
     print("=== end in order ===")
